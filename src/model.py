3e88f0d9ab28bf49b9d6d019ac499133ea2566ff
Pierre Pessarossi <pierre.pessarossi@gmail.com>
2025-05-18 16:00:58 +0200

    feat: add forward pass for MH attention

--- src/model.py
+++ src/model.py
@@ -6,11 +6,12 @@ from torch import nn
 @dataclass
 class BertConfig:
     vocab_size: int = 30522
-    n_embded: int = 768
+    d_model: int = 768
     n_hidden_size: int = 3072
     n_types: int = 2
     block_size: int = 512
     num_layers: int = 12
+    n_head: int = 12
 
 
 class MultiHeadAttention(nn.Module):
@@ -19,20 +20,40 @@ class MultiHeadAttention(nn.Module):
         self.config = config
         self.self = nn.ModuleDict(
             {
-                "query": nn.Linear(config.n_embded, config.n_embded, bias=True),
-                "key": nn.Linear(config.n_embded, config.n_embded, bias=True),
-                "value": nn.Linear(config.n_embded, config.n_embded, bias=True),
+                "query": nn.Linear(config.d_model, config.d_model, bias=True),
+                "key": nn.Linear(config.d_model, config.d_model, bias=True),
+                "value": nn.Linear(config.d_model, config.d_model, bias=True),
             }
         )
         self.output = nn.ModuleDict(
             {
-                "dense": nn.Linear(config.n_embded, config.n_embded, bias=True),
-                "LayerNorm": nn.LayerNorm(config.n_embded),
+                "dense": nn.Linear(config.d_model, config.d_model, bias=True),
+                "LayerNorm": nn.LayerNorm(config.d_model),
             }
         )
 
-    def forward(self, hidden_states, attention_mask):
-        pass
+    def forward(self, x, attention_mask=None):
+        batch, block_size, d_model = x.size()
+        n_head = self.config.n_head
+        d_model_per_head = d_model // n_head
+        q = self.self["query"](x)
+        k = self.self["key"](x)
+        v = self.self["value"](x)
+        q = q.view(batch, block_size, n_head, d_model_per_head).transpose(
+            1, 2
+        )  # [batch, n_head, block_size, d_model_per_head]
+        k = k.view(batch, block_size, n_head, d_model_per_head).transpose(1, 2)
+        v = v.view(batch, block_size, n_head, d_model_per_head).transpose(1, 2)
+        scores = q @ k.transpose(2, 3) / (d_model_per_head**0.5)
+        if attention_mask is not None:
+            scores = scores.masked_fill(attention_mask == 0, float("-inf"))
+        rescaled_scores = scores - scores.max(dim=-1).values.unsqueeze(-1)
+        attention = nn.functional.softmax(rescaled_scores, dim=-1)
+        H = attention @ v
+        H = H.transpose(1, 2).contiguous().view(batch, block_size, d_model)
+        H = self.output["dense"](H)
+        H = self.output["LayerNorm"](x + H)
+        return H
 
 
 class EncoderLayer(nn.Module):
@@ -44,13 +65,13 @@ class EncoderLayer(nn.Module):
         self.attention = MultiHeadAttention(config)
         self.intermediate = nn.ModuleDict(
             {
-                "dense": nn.Linear(config.n_embded, config.n_hidden_size, bias=True),
+                "dense": nn.Linear(config.d_model, config.n_hidden_size, bias=True),
             }
         )
         self.output = nn.ModuleDict(
             {
-                "dense": nn.Linear(config.n_hidden_size, config.n_embded, bias=True),
-                "LayerNorm": nn.LayerNorm(config.n_embded),
+                "dense": nn.Linear(config.n_hidden_size, config.d_model, bias=True),
+                "LayerNorm": nn.LayerNorm(config.d_model),
             }
         )
 
@@ -64,10 +85,10 @@ class BertModel(nn.Module):
         self.config = config
         self.embeddings = nn.ModuleDict(
             {
-                "word_embeddings": nn.Embedding(config.vocab_size, config.n_embded),
-                "position_embeddings": nn.Embedding(config.block_size, config.n_embded),
-                "token_type_embeddings": nn.Embedding(config.n_types, config.n_embded),
-                "LayerNorm": nn.LayerNorm(config.n_embded),
+                "word_embeddings": nn.Embedding(config.vocab_size, config.d_model),
+                "position_embeddings": nn.Embedding(config.block_size, config.d_model),
+                "token_type_embeddings": nn.Embedding(config.n_types, config.d_model),
+                "LayerNorm": nn.LayerNorm(config.d_model),
             }
         )
         self.encoder = nn.ModuleDict(
@@ -79,7 +100,7 @@ class BertModel(nn.Module):
         )
         self.pooler = nn.ModuleDict(
             {
-                "dense": nn.Linear(config.n_embded, config.n_embded, bias=True),
+                "dense": nn.Linear(config.d_model, config.d_model, bias=True),
             }
         )
 
