6af7339caa892ad806489a937e65c4cafc19df83
Pierre Pessarossi <pierre.pessarossi@gmail.com>
2025-06-22 15:59:28 +0200

    feat: add lr scheduler and gradient accumulation

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+import math
 import time
 import numpy as np
 import torch
@@ -72,7 +73,7 @@ elif torch.backends.mps.is_available():
 
 torch.set_float32_matmul_precision("high")
 
-bert_config = BertConfig()
+bert_config = BertConfig(num_layers=1)
 model = BertForMaskedLM(bert_config).to(device)
 
 if torch.cuda.is_available():
@@ -84,31 +85,83 @@ trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Total parameters: {total_params:,}")
 print(f"Trainable parameters: {trainable_params:,}")
 
+
+BATCH_SIZE = 512
+STEP_BATCH_SIZE = 128
+number_grad_accum_steps = BATCH_SIZE // STEP_BATCH_SIZE
+
+assert BATCH_SIZE % STEP_BATCH_SIZE == 0
+
 dataset = ShakespeareDataset(
-    "data/tokenized_train.npy", 64, 128, vocab_size=bert_config.vocab_size
+    "data/tokenized_train.npy", 64, STEP_BATCH_SIZE, vocab_size=bert_config.vocab_size
 )
 
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
-max_steps = 100
-for step in range(max_steps + 1):
-    t0 = time.time()
-    x, y = dataset.next_batch()
-    x = x.to(device)
-    y = y.to(device)
-    with torch.autocast(device_type=device.type, dtype=torch.bfloat16):
-        logits = model(x)
-    optimizer.zero_grad()
-    loss = torch.nn.functional.cross_entropy(
-        logits.view(-1, bert_config.vocab_size), y.view(-1), ignore_index=-100
+
+def get_lr(
+    step: int, warmup_steps: int, max_steps: int, min_lr: float, max_lr: float
+) -> float:
+    if step < warmup_steps:
+        return (step + 1) / warmup_steps * max_lr
+    elif step > max_steps:
+        return min_lr
+    else:
+        step = step - warmup_steps
+        step_decay = max_steps - warmup_steps
+        decay_ratio = step / step_decay
+        assert 0 <= decay_ratio <= 1
+        coef = 1 / 2 * (1 + math.cos(math.pi * decay_ratio))
+        return min_lr + (max_lr - min_lr) * coef
+
+
+def main():
+    max_steps = 100
+    warm_up_steps = 10
+    min_lr = 3e-6
+    max_lr = 1e-5
+    optimizer = torch.optim.AdamW(
+        model.parameters(), lr=3e-4, betas=(0.9, 0.95), eps=1e-8
     )
-    loss.backward()
-    optimizer.step()
-    t1 = time.time()
-    tokens_per_sec = dataset.batch_size * dataset.context_length / (t1 - t0)
-    if step % 10 == 0 or step == max_steps:
-        print(
-            f"Step {step} loss: {loss.item()}, time: {t1 - t0:.2f}s, tokens/sec: {tokens_per_sec:.2f}"
+    for step in range(max_steps + 1):
+        t0 = time.time()
+        loss_accum = 0
+        for _ in range(number_grad_accum_steps):
+            x, y = dataset.next_batch()
+            x = x.to(device)
+            y = y.to(device)
+            with torch.autocast(device_type=device.type, dtype=torch.bfloat16):
+                logits = model(x)
+            loss = torch.nn.functional.cross_entropy(
+                logits.view(-1, bert_config.vocab_size), y.view(-1), ignore_index=-100
+            )
+            loss /= number_grad_accum_steps
+            loss_accum += loss.detach()
+            loss.backward()
+        norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+        lr = get_lr(step, warm_up_steps, max_steps, min_lr, max_lr)
+        for params in optimizer.param_groups:
+            params["lr"] = lr
+        optimizer.step()
+        optimizer.zero_grad()
+        t1 = time.time()
+        tokens_per_sec = (
+            dataset.batch_size
+            * dataset.context_length
+            * number_grad_accum_steps
+            / (t1 - t0)
         )
+        if step % 10 == 0 or step == max_steps:
+            print(
+                f"LR: {lr * 1e5:.4f}e-5, Step {step} loss: {loss_accum}, grad_norm: {norm.item():.4f}, time: {t1 - t0:.2f}s, tokens/sec: {tokens_per_sec:.2f}"
+            )
+
+
+if __name__ == "__main__":
+    main()
+# TODO: add gradient accumulation
+# TODO: add weight decay + fused adamW
+# TODO: add checkpointing with state dict and optimizer + check we can resume training
+# TODO: add parellel training
+
 
 # Total parameters: 110,104,890
 # Trainable parameters: 110,104,890
